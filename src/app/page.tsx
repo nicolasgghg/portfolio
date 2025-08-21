29937a1cc9e4c99b68af0e4125952af5bc45d26b
nicolasgghg <nicolasgghg12@gmail.com>
2025-08-21 14:24:30 -0300

    style: header as modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="">
-        
-      </main>
+      <main className=""></main>
       <Footer />
     </>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex items-center p-4 gap-4">
+    <header className="flex items-center justify-between px-8 py-4 m-1 gap-4 bg-zinc-200 dark:bg-zinc-900 rounded-4xl min-w-[calc(100%-2rem)] md:min-w-1/2 shadow-2xl shadow-gray-600 dark:shadow-blue-400">
       <Image src="/favicon.svg" alt="logo" width={50} height={50} />
-      <h1>Header</h1>
+      <h1>Bernardino, Nicolas Portfolio</h1>
       <ThemeButton />
     </header>
   );
